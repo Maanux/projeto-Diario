2b5511229ae307e216cf658f69f07b4f0ddd399c
emanuel dos santos kanetchny <emanuel.skanis@hotmail.com>
2024-06-23 20:21:18 -0300

    alteracoes

--- Js/Cadastro.js
+++ Js/Cadastro.js
@@ -1,13 +1,8 @@
-// Js/index2.js
-
-// Espera até que o documento esteja completamente carregado
 $(document).ready(function () {
-  // Aplicar a máscara para exigir ao menos 8 caracteres com pelo menos um número
   $("#exampleInputPassword1").mask("0#");
 });
 
 function Cadastrar() {
-  // Validar Usuário
   const usuario = document.getElementById("exampleInputEmail1").value.trim();
   if (!usuario.match(/^[a-zA-Z0-9._-]{3,16}$/)) {
     alert(
@@ -16,24 +11,20 @@ function Cadastrar() {
     return false;
   }
 
-  // Validar Senha
   const senha = document.getElementById("exampleInputPassword1").value.trim();
   if (senha.length < 8) {
     alert("Senha inválida. Deve ter pelo menos 8 caracteres.");
     return false;
   }
 
-  // Salvar dados no localStorage
   localStorage.setItem("usuario", usuario);
   localStorage.setItem("senha", senha);
 
-  // Redirecionar para a página de cadastro
   var urlCadastra = "Login.html";
   window.location.href = urlCadastra;
 }
 
 function Cancelar() {
-  // Cancelar ação de cadastro e redirecionar para a página inicial
   var urlCancela = "Inicio.html";
   window.location.href = urlCancela;
 }
--- Js/Login.js
+++ Js/Login.js
@@ -1,27 +1,22 @@
 function logar() {
-  // Validar Usuário
   const usuario = document.getElementById("exampleInputEmail1").value.trim();
   if (!usuario.match(/^[a-zA-Z0-9._-]{3,16}$/)) {
     alert("Usuário inválido.");
     return false;
   }
 
-  // Validar Senha
   const senha = document.getElementById("exampleInputPassword1").value.trim();
   if (senha.length < 8) {
     alert("Senha inválida.");
     return false;
   }
 
-  // Recuperar dados do localStorage
   const usuarioArmazenado = localStorage.getItem("usuario");
   const senhaArmazenada = localStorage.getItem("senha");
 
-  // Verificar se os dados de login correspondem aos armazenados
   if (usuario === usuarioArmazenado && senha === senhaArmazenada) {
     alert("Login realizado com sucesso!");
 
-    // Redirecionar para a página de login
     var urlLoga = "Principal.html";
     window.location.href = urlLoga;
   } else {
@@ -30,7 +25,6 @@ function logar() {
 }
 
 function Cancelar() {
-  // Cancelar ação de login e redirecionar para a página inicial
   var urlCancela = "Inicio.html";
   window.location.href = urlCancela;
 }
